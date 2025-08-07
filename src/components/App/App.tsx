2cb15ae4b36115ceaaf9cec96a478f41c7770ed2
Yurii <voronich97@gmail.com>
2025-08-07 17:55:07 +0300

    add components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,18 @@
-import css from './App.module.css'
+import CafeInfo from "../CafeInfo/CafeInfo";
+import Notification from "../Notification/Notification";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStatus from "../VoteStatus/VoteStatus";
+import css from "./App.module.css";
 
 function App() {
-
   return (
-<div className={css.app}></div>)
-
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions />
+      <VoteStatus />
+      <Notification />
+    </div>
+  );
 }
 
-export default App
+export default App;
